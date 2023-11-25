5ac21465509d68a45027e6afc424dce41cdb178e
Anant077 <anantjejani7@gmail.com>
2023-11-25 16:45:59 +0530

    lab06 commit

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get(
+    '/',
+    [HomeController::class, 'index']
+)->name('profile');
+
+Route::get(
+    '/contact',
+    [HomeController::class, 'contact']
+)->name('contact');
+
+Route::post(
+    '/contact',
+    [HomeController::class, 'create']
+)->name('contact.create');
+
+Route::get(
+    '/person/{name?}',
+    [PersonController::class, 'index']
+)->name('person.index');
+
+Route::resources([
+    'person'=> PersonController::class
+]);
+
+// Route::resource('person', PersonController::class);
+
+Route::resource('person', PersonController::class)->only(['index','create']);
+
+Route::resource('person', PersonController::class)->except(['create']);
+
+Route::resource('person', PersonController::class)
+    ->only(['index','create'])
+    ->names([
+        'index' => 'person.superindex',
+        'create' => 'person.supercreate'
+    ]);
+
+
+
